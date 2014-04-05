0baea252d4f346ae3e0daf08ec52470d2a7c6dbd
Alex Zylstra <alex@azylstra.net>
2014-04-05 17:35:09 -0400

    fixing port

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port, debug=True)
