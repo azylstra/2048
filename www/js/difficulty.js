d66c2106157806d171427c72bca7df2842bc61f2
Alex Zylstra <alex@azylstra.net>
2014-04-05 17:11:11 -0400

    cleanup

--- www/js/difficulty.js
+++
@@ -1,7 +0,0 @@
-$('body').on('click', '.btn-group button', function (e) {
-    $(this).addClass('active');
-    $(this).siblings().removeClass('active');
-
-	var diff = Math.round(100*(1-parseFloat($(this).data("value"))));
-	$('#perc').html('p + p &rarr; D (' + diff + '%)');
-});
